08f0b4d6227ec4567007fb3e7729275c134c1d9b
ivanmolcan <ico@molcan.sk>
2021-02-16 18:50:51 +0100

    16.2.21 -1

--- Udemy/QuizApp/app/src/main/java/sk/ivanmolcan/quizapp/Question.kt
+++ Udemy/QuizApp/app/src/main/java/sk/ivanmolcan/quizapp/Question.kt
@@ -1,4 +1,9 @@
 package sk.ivanmolcan.quizapp
 
-class Question {
+data class Question(
+        val id: Int, val question: String, val image: Int, val optionOne: String,
+        val optionTwo: String, val optionThree: String, val optionFour: String,
+        val correctAnswer: Int
+                    ) {
+
 }

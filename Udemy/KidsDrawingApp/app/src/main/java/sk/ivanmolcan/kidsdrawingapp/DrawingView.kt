b52804ec17bc056f87b2b6d9dbb004bbe4ad7c86
ivanmolcan <ico@molcan.sk>
2021-02-17 21:39:04 +0100

    17.2.21 -4

--- Udemy/KidsDrawingApp/app/src/main/java/sk/ivanmolcan/kidsdrawingapp/DrawingView.kt
+++ Udemy/KidsDrawingApp/app/src/main/java/sk/ivanmolcan/kidsdrawingapp/DrawingView.kt
@@ -1,10 +1,7 @@
 package sk.ivanmolcan.kidsdrawingapp
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
@@ -14,10 +11,24 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var mCanvasBitmap: Bitmap? = null
     private var mDrawPaint: Paint? = null
     private var mCanvasPaint: Paint? = null
-    private var mBrushSize: Float? = 0.toFloat()
+    private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
+    private var canvas: Canvas? = null
 
+    init{
+        setUpDrawing()
+    }
 
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
+    }
 
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float ): Path() {

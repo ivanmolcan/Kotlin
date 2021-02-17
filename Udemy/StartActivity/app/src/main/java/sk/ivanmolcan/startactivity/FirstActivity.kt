34827b986dcff95f32492e4eaa84dda6c08086b3
ivanmolcan <ico@molcan.sk>
2021-02-17 20:05:16 +0100

    17.2.21 -2

--- Udemy/StartActivity/app/src/main/java/sk/ivanmolcan/startactivity/FirstActivity.kt
+++ Udemy/StartActivity/app/src/main/java/sk/ivanmolcan/startactivity/FirstActivity.kt
@@ -1,11 +1,24 @@
 package sk.ivanmolcan.startactivity
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sk.ivanmolcan.startactivity.databinding.ActivityFirstBinding
+import sk.ivanmolcan.startactivity.databinding.ActivityMainBinding
 
 class FirstActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFirstBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first)
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFinish.setOnClickListener{
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+
     }
 }
--- Udemy/StartActivity/app/src/main/java/sk/ivanmolcan/startactivity/SecondActivity.kt
+++ Udemy/StartActivity/app/src/main/java/sk/ivanmolcan/startactivity/SecondActivity.kt
@@ -1,11 +1,28 @@
 package sk.ivanmolcan.startactivity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sk.ivanmolcan.startactivity.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSubmit.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra(MainActivity.NAME, binding.etName.text.toString())
+            intent.putExtra(MainActivity.EMAIL, binding.etEmail.text.toString())
+
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+
     }
 }

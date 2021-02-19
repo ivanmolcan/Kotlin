716cf580694179d34144faefdc767e9df145a1bc
ivanmolcan <ico@molcan.sk>
2021-02-19 21:24:49 +0100

    19.2.21 -2

---
+++ Udemy/CustomDialog/app/src/main/java/sk/ivanmolcan/customdialog/MainActivity.kt
@@ -0,0 +1,11 @@
+package sk.ivanmolcan.customdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- Udemy/KidsDrawingApp/app/src/main/java/sk/ivanmolcan/kidsdrawingapp/MainActivity.kt
+++ Udemy/KidsDrawingApp/app/src/main/java/sk/ivanmolcan/kidsdrawingapp/MainActivity.kt
@@ -1,16 +1,25 @@
  package sk.ivanmolcan.kidsdrawingapp
 
+import android.Manifest
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Gallery
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 import sk.ivanmolcan.kidsdrawingapp.databinding.ActivityMainBinding
+import java.lang.Exception
 
  class MainActivity : AppCompatActivity() {
 
@@ -31,6 +40,39 @@ import sk.ivanmolcan.kidsdrawingapp.databinding.ActivityMainBinding
 
          ib_brush.setOnClickListener {
              showBrushSizeChooserDialog()
+         }
+
+         ib_gallery.setOnClickListener {
+             if(isReadStorageAllowed()){
+                val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                 startActivityForResult(pickPhotoIntent, GALLERY)
+
+             }else{
+                 requestStoragePermission()
+             }
+         }
+
+         ib_undo.setOnClickListener {
+             drawing_view.onClickUndo()
+         }
+     }
+
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+         super.onActivityResult(requestCode, resultCode, data)
+         if(resultCode == Activity.RESULT_OK){
+            if(requestCode == GALLERY){
+                try {
+                    if(data!!.data !=null){
+                        iv_background.visibility = View.VISIBLE
+                        iv_background.setImageURI(data.data)
+                    }else{
+                        Toast.makeText(this,"Error in parsing", Toast.LENGTH_SHORT).show()
+                    }
+                }catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
          }
      }
 
@@ -74,5 +116,36 @@ import sk.ivanmolcan.kidsdrawingapp.databinding.ActivityMainBinding
             )
             mImageButtonCurrentPaint = view
         }
+     }
+
+     private fun requestStoragePermission(){
+         if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                         arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                                 Manifest.permission.WRITE_EXTERNAL_STORAGE).toString())){
+            Toast.makeText(this,"Need permission", Toast.LENGTH_LONG).show()
+         }
+         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+                 Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+     }
+
+     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"Permission granted", Toast.LENGTH_LONG).show()
+            }
+        }else{
+            Toast.makeText(this,"You just denied permission", Toast.LENGTH_LONG).show()
+        }
+     }
+
+     private fun isReadStorageAllowed():Boolean{
+         val result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+         return result == PackageManager.PERMISSION_GRANTED
+     }
+
+     companion object{
+         private const val STORAGE_PERMISSION_CODE = 1
+         private const val GALLERY = 2
      }
  }
